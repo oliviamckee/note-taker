e6b49024edc45eb78dbb5f06c569d7cf2845f741
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-07 17:31:55 -0500

    first api route works

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Note Taker Starter Code
+# Note Taker App
+
+## Description 
+I created this note taking app using Node.JS and the Express.js application. 
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+Navigate to the deployed [heroku page]() to begin taking notes. 
+
+## Usage 
+To add a note, click on the + icon in the top right corner. To save a note, click on the save icon in the top right corner. 
+
+## License
+This application is covered under the MIT License. 
+
+## Questions 
+My Github profile: [oliviamckee](https://github.com/oliviamckee)
+Please email me with any questions, comments, or concerns at olivia.mckee97@gmail.com.
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const notes = require('./db/db.json');
+
+const app = express();
+
+// api routes 
+// GET /api/notes should read the db.json file and return all saved notes as json
+// POST /api/notes should recieve a new note to save on the request body, add it ot the db, json file, then return the new note to the client 
+// find a way to give each note a unique id when it's saved (look into npm packages that could do this)
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+// html routes 
+// GET /notes should return the notes.html file
+// GET * should return the index.html file
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
